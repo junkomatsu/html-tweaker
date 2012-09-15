d255cf0aceb17a712dd0e51cbf5f0b165e0a6d8d
Jun Komatsu <jun@uniba.jp>
2012-09-15 11:48:29 +0900

    dom iteration, append init event

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-window.alert('html-tweaker!!');
+try {
+  window.addEventListener('load', init, false);
+} catch(e) {
+  window.attachEvent('onload', init);
+}
+
+
+function init() {
+  window.alert('html-tweaker!!');
+
+  var elements = document.getElementByTagName("*");
+  var length = elements.length;
+  while(i < length) {
+    var element = elements[i++];
+    element.style.backgroundColor = randomColor();
+    element.style.borderColor = randomColor();
+    element.style.borderWidth = '1px';
+  }
+}
+
+function randomColor() {
+  return '#ff0000';
+}
